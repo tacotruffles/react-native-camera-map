527f1d5388dc59ebd7c9722353ab75bb6490b8ef
John Dawes <jdawes@bluebitmedia.com>
2018-06-19 19:54:16 -0600

    First commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends Component {
+  
+
+    state = {
+      placeName: ""
+    };
+
+    placeNameChangedHandler = (val) => {
+      this.setState({
+        placeName: val
+      })
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <TextInput
+            style={{width: 300, borderColor: "black", borderWidth: 1}}
+            placeholder="Hello World"
+            value={this.state.placeName} 
+            onChangeText={this.placeNameChangedHandler}
+          />
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# react-native-camera-map
+
+React Native Docs:
+https://facebook.github.io/react-native/docs/tutorial.html
