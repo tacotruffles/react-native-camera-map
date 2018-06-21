0e54e9fa4d57ffea292877ac2d3ccf1f3928a4c4
John Dawes <jdawes@bluebitmedia.com>
2018-06-21 15:54:03 -0600

    Modal and controls

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
-import placeImage from './src/assets/beautiful-place.jpg';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 
 export default class App extends Component {
   
   state = {
     //places: ["San Diego","Los Angeles","Madrid","Lisbon","Oviedo","Milan","San Francisco","New York","Ipswitch","Mobile","Miami","Charlotte","San Jose","Seattle","Portland","Dallas","Austin","Houston"]
-    places: []
+    places: [],
+    placeSelected: null
   };
 
   
@@ -20,7 +21,9 @@ export default class App extends Component {
       const newPlace = prevState.places.concat({
         key: Math.random().toString(), 
         name: placeName,
-        image: placeImage
+        image: {
+          uri: "https://c2.staticflickr.com/2/1489/24690903966_8a20c137fa_n.jpg"//placeImage
+        }
       });
       
       return {
@@ -29,21 +32,37 @@ export default class App extends Component {
     })
   }
 
-  placeDeletedHandler = key => {
+  placeDeletedHandler = () => {
     this.setState(prevState => {
-      return{
-        places: prevState.places.filter((place, i) => {
-          return place.key !== key;
-        })
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== this.state.placeSelected.key;
+        }),
+        placeSelected: null
       }
     });
+  }
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        placeSelected: prevState.places.find(place => {
+          return place.key === key;
+        })
+      }
+    })
+  }
+
+  modalClosedHandler = () => {
+    this.setState({placeSelected: null});
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail selectedPlace={this.state.placeSelected} onItemDeleted={this.placeDeletedHandler} onModalClosed={this.modalClosedHandler}/>
         <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler}/>
+        <PlaceList places={this.state.places} onItemSelected={this.placeSelectedHandler}/>
       </View>
     );
   }
---
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Modal, View, Image, Text, Button } from 'react-native';
+
+const placeDetail = (props) => {
+
+    let modalContent = null;
+
+    if (props.selectedPlace) {
+        modalContent = (
+            <View>
+                <Image source={props.selectedPlace.image} style={styles.placeImage}/>
+                <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+            </View>
+        );
+    }
+
+    return (
+        <Modal visible={props.selectedPlace !== null} animationType="slide" onRequestClose={() => {}} >
+        <View style={styles.modalContainer}>
+            {modalContent}
+            <View>
+                <Button title="Delete" color="red" onPress={props.onItemDeleted}/>
+                <Button title="Close" onPress={props.onModalClosed}/>
+            </View>
+        </View>
+    </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        margin: 30
+    },
+    placeImage: {
+        width: "100%",
+        height: 200
+    },
+    placeName: {
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 28
+    }
+});
+
+export default placeDetail;
