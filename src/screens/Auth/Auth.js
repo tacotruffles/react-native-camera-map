507a91bc995c5c7ccd5e7a97949babe89e5aa149
John Dawes <jdawes@bluebitmedia.com>
2018-06-29 14:13:27 -0600

    Login Screen - started flexbox styling

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 
-import startMainTabs from '../MainTabs/startMainTabs';
+import startMainTabs from "../MainTabs/startMainTabs";
 
 class AuthScreen extends Component {
+	loginHandler = () => {
+		startMainTabs();
+	};
 
-    loginHandler = () => {
-        startMainTabs();
-    }
-
-    render() {
-        return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler} />
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Please Log In</Text>
+				<Button title="Switch to Login" />
+				<TextInput placeholder="Your E-mail Address" />
+				<TextInput placeholder="Password" />
+				<TextInput placeholder="Confirm Password" />
+				<Button title="Submit" onPress={this.loginHandler} />
+			</View>
+		);
+	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		//vertical alignment
+		justifyContent: "center",
+		// Horizontal alignment
+		alignItems: "center"
+	}
+});
 
 export default AuthScreen;
