c0094fb2fba17f8b0155b25d147d064ea1173eac
John Dawes <jdawes@bluebitmedia.com>
2018-06-29 10:09:31 -0600

    Code clean up

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,15 +1,15 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 
-export const addPlace = (placeName) => {
-    return {
-        type: actionTypes.ADD_PLACE,
-        placeName: placeName
-    }
+export const addPlace = placeName => {
+	return {
+		type: actionTypes.ADD_PLACE,
+		placeName: placeName
+	};
 };
 
-export const deletePlace = (key) => {
-    return {
-        type: actionTypes.DEL_PLACE,
-        placeKey: key
-    }
+export const deletePlace = key => {
+	return {
+		type: actionTypes.DEL_PLACE,
+		placeKey: key
+	};
 };
--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,32 +1,32 @@
-import * as actionTypes from '../actions/actionTypes';
+import * as actionTypes from "../actions/actionTypes";
 
 const initialState = {
-    places: []
-}
+	places: []
+};
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case actionTypes.ADD_PLACE:
-            return {
-                ...state,
-                places: state.places.concat({
-                    key: Math.random().toString(), 
-                    name: action.placeName,
-                    image: {
-                    uri: "https://c1.staticflickr.com/9/8358/8288031982_e8391a4475_n.jpg" //"https://c2.staticflickr.com/2/1489/24690903966_8a20c137fa_n.jpg"
-                    }
-                })
-            }
-        case actionTypes.DEL_PLACE:
-            return {
-                ...state,
-                places: state.places.filter(place => {
-                    return place.key !== action.placeKey;
-                })
-            }
-        default:
-            return state;
-    }
-}
+	switch (action.type) {
+		case actionTypes.ADD_PLACE:
+			return {
+				...state,
+				places: state.places.concat({
+					key: Math.random().toString(),
+					name: action.placeName,
+					image: {
+						uri: "https://c1.staticflickr.com/9/8358/8288031982_e8391a4475_n.jpg" //"https://c2.staticflickr.com/2/1489/24690903966_8a20c137fa_n.jpg"
+					}
+				})
+			};
+		case actionTypes.DEL_PLACE:
+			return {
+				...state,
+				places: state.places.filter(place => {
+					return place.key !== action.placeKey;
+				})
+			};
+		default:
+			return state;
+	}
+};
 
 export default reducer;
